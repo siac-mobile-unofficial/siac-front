e947066b04f6917ac619bc392e6f2eb1ff1c6745
VitaminaNescau <vitorjss82@gmail.com>
2023-12-05 03:20:14 -0300

    [TEST] value for test

---
+++ app/utils/teste.js
@@ -0,0 +1,856 @@
+export const pointBusPositionGoing = [
+  {
+    name: "Estacionamento PAF I",
+    latitude: -13.001857935829568,
+    longitude: -38.50700618419231,
+    bus: ["EXPRESSO", "B1", "B2", "B3", "B4", "B5"],
+  },
+  {
+    name: "Av. Garibaldi",
+    latitude: -12.999244911967649,
+    longitude: -38.50590616412396,
+    bus: ["EXPRESSO"],
+  },
+  {
+    name: "Faculdade de Arquitetura",
+    latitude: -12.997182537250083,
+    longitude: -38.5089103976332,
+    bus: ["EXPRESSO"],
+  },
+  {
+    name: "Politécnica",
+    latitude: -12.998730923655947,
+    longitude: -38.5117837954863,
+    bus: ["EXPRESSO"],
+  },
+  {
+    name: "Creche",
+    latitude: -12.994740129031742,
+    longitude: -38.517503374266084,
+    bus: ["EXPRESSO"],
+  },
+];
+
+export const routerExpress = [
+  {
+    longitude: -38.506997,
+    latitude: -13.001805,
+  },
+  {
+    longitude: -38.506999,
+    latitude: -13.001803,
+  },
+  {
+    longitude: -38.507434,
+    latitude: -13.002225,
+  },
+  {
+    longitude: -38.507454,
+    latitude: -13.002271,
+  },
+  {
+    longitude: -38.507176,
+    latitude: -13.002558,
+  },
+  {
+    longitude: -38.507206,
+    latitude: -13.002586,
+  },
+  {
+    longitude: -38.507185,
+    latitude: -13.002666,
+  },
+  {
+    longitude: -38.50696,
+    latitude: -13.002881,
+  },
+  {
+    longitude: -38.507878,
+    latitude: -13.00381,
+  },
+  {
+    longitude: -38.508766,
+    latitude: -13.004702,
+  },
+  {
+    longitude: -38.509055,
+    latitude: -13.005001,
+  },
+  {
+    longitude: -38.50922,
+    latitude: -13.005206,
+  },
+  {
+    longitude: -38.50941,
+    latitude: -13.005566,
+  },
+  {
+    longitude: -38.509458,
+    latitude: -13.005849,
+  },
+  {
+    longitude: -38.509458,
+    latitude: -13.005849,
+  },
+  {
+    longitude: -38.509469,
+    latitude: -13.006069,
+  },
+  {
+    longitude: -38.509349,
+    latitude: -13.006269,
+  },
+  {
+    longitude: -38.509277,
+    latitude: -13.006303,
+  },
+  {
+    longitude: -38.509233,
+    latitude: -13.006298,
+  },
+  {
+    longitude: -38.509232,
+    latitude: -13.006264,
+  },
+  {
+    longitude: -38.509232,
+    latitude: -13.006264,
+  },
+  {
+    longitude: -38.509233,
+    latitude: -13.006298,
+  },
+  {
+    longitude: -38.50917,
+    latitude: -13.006259,
+  },
+  {
+    longitude: -38.509069,
+    latitude: -13.006079,
+  },
+  {
+    longitude: -38.509055,
+    latitude: -13.005897,
+  },
+  {
+    longitude: -38.509055,
+    latitude: -13.005897,
+  },
+  {
+    longitude: -38.509072,
+    latitude: -13.00543,
+  },
+  {
+    longitude: -38.509023,
+    latitude: -13.005166,
+  },
+  {
+    longitude: -38.508766,
+    latitude: -13.004702,
+  },
+  {
+    longitude: -38.507878,
+    latitude: -13.00381,
+  },
+  {
+    longitude: -38.505727,
+    latitude: -13.001627,
+  },
+  {
+    longitude: -38.505644,
+    latitude: -13.001561,
+  },
+  {
+    longitude: -38.505644,
+    latitude: -13.001561,
+  },
+  {
+    longitude: -38.505507,
+    latitude: -13.001469,
+  },
+  {
+    longitude: -38.505423,
+    latitude: -13.00144,
+  },
+  {
+    longitude: -38.50534,
+    latitude: -13.00143,
+  },
+  {
+    longitude: -38.505211,
+    latitude: -13.00144,
+  },
+  {
+    longitude: -38.505058,
+    latitude: -13.001491,
+  },
+  {
+    longitude: -38.505009,
+    latitude: -13.001608,
+  },
+  {
+    longitude: -38.504981,
+    latitude: -13.001639,
+  },
+  {
+    longitude: -38.504594,
+    latitude: -13.001929,
+  },
+  {
+    longitude: -38.504443,
+    latitude: -13.00201,
+  },
+  {
+    longitude: -38.504388,
+    latitude: -13.002023,
+  },
+  {
+    longitude: -38.504269,
+    latitude: -13.00202,
+  },
+  {
+    longitude: -38.503664,
+    latitude: -13.00246,
+  },
+  {
+    longitude: -38.503664,
+    latitude: -13.00246,
+  },
+  {
+    longitude: -38.503331,
+    latitude: -13.002701,
+  },
+  {
+    longitude: -38.503362,
+    latitude: -13.002822,
+  },
+  {
+    longitude: -38.503358,
+    latitude: -13.002919,
+  },
+  {
+    longitude: -38.503232,
+    latitude: -13.003224,
+  },
+  {
+    longitude: -38.503199,
+    latitude: -13.003258,
+  },
+  {
+    longitude: -38.503038,
+    latitude: -13.003322,
+  },
+  {
+    longitude: -38.503026,
+    latitude: -13.003389,
+  },
+  {
+    longitude: -38.503113,
+    latitude: -13.003591,
+  },
+  {
+    longitude: -38.503022,
+    latitude: -13.004091,
+  },
+  {
+    longitude: -38.502888,
+    latitude: -13.004207,
+  },
+  {
+    longitude: -38.502769,
+    latitude: -13.00493,
+  },
+  {
+    longitude: -38.502711,
+    latitude: -13.005237,
+  },
+  {
+    longitude: -38.502668,
+    latitude: -13.005382,
+  },
+  {
+    longitude: -38.502513,
+    latitude: -13.005697,
+  },
+  {
+    longitude: -38.502388,
+    latitude: -13.005888,
+  },
+  {
+    longitude: -38.502295,
+    latitude: -13.005999,
+  },
+  {
+    longitude: -38.502165,
+    latitude: -13.006119,
+  },
+  {
+    longitude: -38.501902,
+    latitude: -13.006322,
+  },
+  {
+    longitude: -38.501649,
+    latitude: -13.006453,
+  },
+  {
+    longitude: -38.501068,
+    latitude: -13.006612,
+  },
+  {
+    longitude: -38.501068,
+    latitude: -13.006612,
+  },
+  {
+    longitude: -38.500692,
+    latitude: -13.00671,
+  },
+  {
+    longitude: -38.50057,
+    latitude: -13.006672,
+  },
+  {
+    longitude: -38.500522,
+    latitude: -13.006612,
+  },
+  {
+    longitude: -38.500512,
+    latitude: -13.006571,
+  },
+  {
+    longitude: -38.500547,
+    latitude: -13.006499,
+  },
+  {
+    longitude: -38.500753,
+    latitude: -13.006365,
+  },
+  {
+    longitude: -38.500698,
+    latitude: -13.006378,
+  },
+  {
+    longitude: -38.500698,
+    latitude: -13.006378,
+  },
+  {
+    longitude: -38.501353,
+    latitude: -13.006205,
+  },
+  {
+    longitude: -38.501563,
+    latitude: -13.006124,
+  },
+  {
+    longitude: -38.501689,
+    latitude: -13.006055,
+  },
+  {
+    longitude: -38.501903,
+    latitude: -13.005903,
+  },
+  {
+    longitude: -38.502074,
+    latitude: -13.005759,
+  },
+  {
+    longitude: -38.502274,
+    latitude: -13.005519,
+  },
+  {
+    longitude: -38.502352,
+    latitude: -13.005271,
+  },
+  {
+    longitude: -38.502556,
+    latitude: -13.003796,
+  },
+  {
+    longitude: -38.502717,
+    latitude: -13.002843,
+  },
+  {
+    longitude: -38.502925,
+    latitude: -13.002561,
+  },
+  {
+    longitude: -38.503064,
+    latitude: -13.002459,
+  },
+  {
+    longitude: -38.503171,
+    latitude: -13.002417,
+  },
+  {
+    longitude: -38.503535,
+    latitude: -13.002143,
+  },
+  {
+    longitude: -38.503648,
+    latitude: -13.002292,
+  },
+  {
+    longitude: -38.503807,
+    latitude: -13.002216,
+  },
+  {
+    longitude: -38.503857,
+    latitude: -13.002172,
+  },
+  {
+    longitude: -38.503924,
+    latitude: -13.002077,
+  },
+  {
+    longitude: -38.504234,
+    latitude: -13.001869,
+  },
+  {
+    longitude: -38.504386,
+    latitude: -13.001801,
+  },
+  {
+    longitude: -38.504838,
+    latitude: -13.001492,
+  },
+  {
+    longitude: -38.504939,
+    latitude: -13.001391,
+  },
+  {
+    longitude: -38.504987,
+    latitude: -13.001323,
+  },
+  {
+    longitude: -38.5052,
+    latitude: -13.001198,
+  },
+  {
+    longitude: -38.505372,
+    latitude: -13.000924,
+  },
+  {
+    longitude: -38.505711,
+    latitude: -13.000282,
+  },
+  {
+    longitude: -38.505921,
+    latitude: -12.999891,
+  },
+  {
+    longitude: -38.505915,
+    latitude: -12.999817,
+  },
+  {
+    longitude: -38.505891,
+    latitude: -12.999657,
+  },
+  {
+    longitude: -38.505911,
+    latitude: -12.999599,
+  },
+  {
+    longitude: -38.505879,
+    latitude: -12.99952,
+  },
+  {
+    longitude: -38.505874,
+    latitude: -12.999466,
+  },
+  {
+    longitude: -38.506585,
+    latitude: -12.997548,
+  },
+  {
+    longitude: -38.506733,
+    latitude: -12.997096,
+  },
+  {
+    longitude: -38.506778,
+    latitude: -12.996884,
+  },
+  {
+    longitude: -38.506802,
+    latitude: -12.996678,
+  },
+  {
+    longitude: -38.50689,
+    latitude: -12.995703,
+  },
+  {
+    longitude: -38.50689,
+    latitude: -12.995703,
+  },
+  {
+    longitude: -38.506907,
+    latitude: -12.99551,
+  },
+  {
+    longitude: -38.506852,
+    latitude: -12.995481,
+  },
+  {
+    longitude: -38.506815,
+    latitude: -12.995483,
+  },
+  {
+    longitude: -38.506752,
+    latitude: -12.995521,
+  },
+  {
+    longitude: -38.506617,
+    latitude: -12.996252,
+  },
+  {
+    longitude: -38.50656,
+    latitude: -12.996512,
+  },
+  {
+    longitude: -38.506496,
+    latitude: -12.996681,
+  },
+  {
+    longitude: -38.506391,
+    latitude: -12.996809,
+  },
+  {
+    longitude: -38.506391,
+    latitude: -12.996809,
+  },
+  {
+    longitude: -38.506347,
+    latitude: -12.996886,
+  },
+  {
+    longitude: -38.507077,
+    latitude: -12.99676,
+  },
+  {
+    longitude: -38.507289,
+    latitude: -12.996748,
+  },
+  {
+    longitude: -38.507623,
+    latitude: -12.996747,
+  },
+  {
+    longitude: -38.507833,
+    latitude: -12.996763,
+  },
+  {
+    longitude: -38.508168,
+    latitude: -12.996819,
+  },
+  {
+    longitude: -38.508418,
+    latitude: -12.996911,
+  },
+  {
+    longitude: -38.509471,
+    latitude: -12.997651,
+  },
+  {
+    longitude: -38.509555,
+    latitude: -12.997727,
+  },
+  {
+    longitude: -38.509555,
+    latitude: -12.997727,
+  },
+  {
+    longitude: -38.51078,
+    latitude: -12.998848,
+  },
+  {
+    longitude: -38.510985,
+    latitude: -12.99902,
+  },
+  {
+    longitude: -38.511126,
+    latitude: -12.999069,
+  },
+  {
+    longitude: -38.511202,
+    latitude: -12.999074,
+  },
+  {
+    longitude: -38.511365,
+    latitude: -12.999054,
+  },
+  {
+    longitude: -38.511499,
+    latitude: -12.99899,
+  },
+  {
+    longitude: -38.511941,
+    latitude: -12.998671,
+  },
+  {
+    longitude: -38.51211,
+    latitude: -12.998521,
+  },
+  {
+    longitude: -38.512553,
+    latitude: -12.998037,
+  },
+  {
+    longitude: -38.51279,
+    latitude: -12.997916,
+  },
+  {
+    longitude: -38.512785,
+    latitude: -12.997905,
+  },
+  {
+    longitude: -38.512785,
+    latitude: -12.997905,
+  },
+  {
+    longitude: -38.51279,
+    latitude: -12.997916,
+  },
+  {
+    longitude: -38.512945,
+    latitude: -12.997882,
+  },
+  {
+    longitude: -38.513304,
+    latitude: -12.997888,
+  },
+  {
+    longitude: -38.513701,
+    latitude: -12.997898,
+  },
+  {
+    longitude: -38.514011,
+    latitude: -12.997833,
+  },
+  {
+    longitude: -38.514357,
+    latitude: -12.997796,
+  },
+  {
+    longitude: -38.514357,
+    latitude: -12.997796,
+  },
+  {
+    longitude: -38.514428,
+    latitude: -12.997788,
+  },
+  {
+    longitude: -38.515017,
+    latitude: -12.997538,
+  },
+  {
+    longitude: -38.515376,
+    latitude: -12.997354,
+  },
+  {
+    longitude: -38.515489,
+    latitude: -12.99729,
+  },
+  {
+    longitude: -38.51581,
+    latitude: -12.997047,
+  },
+  {
+    longitude: -38.51595,
+    latitude: -12.996892,
+  },
+  {
+    longitude: -38.516071,
+    latitude: -12.99671,
+  },
+  {
+    longitude: -38.516328,
+    latitude: -12.996249,
+  },
+  {
+    longitude: -38.516559,
+    latitude: -12.995701,
+  },
+  {
+    longitude: -38.516677,
+    latitude: -12.995486,
+  },
+  {
+    longitude: -38.516739,
+    latitude: -12.995421,
+  },
+  {
+    longitude: -38.516937,
+    latitude: -12.99527,
+  },
+  {
+    longitude: -38.518369,
+    latitude: -12.994204,
+  },
+  {
+    longitude: -38.518369,
+    latitude: -12.994204,
+  },
+  {
+    longitude: -38.518632,
+    latitude: -12.994035,
+  },
+  {
+    longitude: -38.518632,
+    latitude: -12.994035,
+  },
+  {
+    longitude: -38.519016,
+    latitude: -12.993801,
+  },
+  {
+    longitude: -38.519016,
+    latitude: -12.993801,
+  },
+  {
+    longitude: -38.519118,
+    latitude: -12.993724,
+  },
+  {
+    longitude: -38.519143,
+    latitude: -12.993678,
+  },
+  {
+    longitude: -38.519192,
+    latitude: -12.993434,
+  },
+  {
+    longitude: -38.519192,
+    latitude: -12.993434,
+  },
+  {
+    longitude: -38.519263,
+    latitude: -12.993164,
+  },
+  {
+    longitude: -38.519354,
+    latitude: -12.993057,
+  },
+  {
+    longitude: -38.519354,
+    latitude: -12.993057,
+  },
+  {
+    longitude: -38.520148,
+    latitude: -12.992378,
+  },
+  {
+    longitude: -38.520148,
+    latitude: -12.992378,
+  },
+  {
+    longitude: -38.520372,
+    latitude: -12.992184,
+  },
+  {
+    longitude: -38.520481,
+    latitude: -12.991234,
+  },
+  {
+    longitude: -38.520465,
+    latitude: -12.991142,
+  },
+  {
+    longitude: -38.520143,
+    latitude: -12.990365,
+  },
+  {
+    longitude: -38.520143,
+    latitude: -12.990365,
+  },
+  {
+    longitude: -38.52013,
+    latitude: -12.990334,
+  },
+  {
+    longitude: -38.520429,
+    latitude: -12.990216,
+  },
+  {
+    longitude: -38.520463,
+    latitude: -12.990212,
+  },
+  {
+    longitude: -38.520518,
+    latitude: -12.99023,
+  },
+  {
+    longitude: -38.520603,
+    latitude: -12.990401,
+  },
+  {
+    longitude: -38.520603,
+    latitude: -12.990401,
+  },
+  {
+    longitude: -38.521461,
+    latitude: -12.99197,
+  },
+  {
+    longitude: -38.521461,
+    latitude: -12.99197,
+  },
+  {
+    longitude: -38.52152,
+    latitude: -12.992076,
+  },
+  {
+    longitude: -38.521077,
+    latitude: -12.992309,
+  },
+  {
+    longitude: -38.521077,
+    latitude: -12.992309,
+  },
+  {
+    longitude: -38.520773,
+    latitude: -12.992458,
+  },
+  {
+    longitude: -38.520444,
+    latitude: -12.992422,
+  },
+  {
+    longitude: -38.520217,
+    latitude: -12.99242,
+  },
+];
+export const bus = [
+  {
+    name: "B1",
+    router: null,
+    point: pointBusPositionGoing.filter((point) =>
+      point.bus.filter((name) => name == "B1"),
+    ),
+  },
+  {
+    name: "B2",
+    router: null,
+    point: pointBusPositionGoing.filter((point) =>
+      point.bus.filter((name) => name == "B2"),
+    ),
+  },
+  {
+    name: "B3",
+    router: null,
+    point: pointBusPositionGoing.filter((point) =>
+      point.bus.filter((name) => name == "B3"),
+    ),
+  },
+  {
+    name: "B4",
+    router: null,
+    point: pointBusPositionGoing.filter((point) =>
+      point.bus.filter((name) => name == "B4"),
+    ),
+  },
+  {
+    name: "EXPRESSO",
+    router: routerExpress,
+    point: pointBusPositionGoing.filter((point) =>
+      point.bus.filter((name) => name == "EXPRESSO"),
+    ),
+  },
+];
